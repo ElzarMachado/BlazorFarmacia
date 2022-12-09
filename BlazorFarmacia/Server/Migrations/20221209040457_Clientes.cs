9ef7c10e92fea1a8eff90342c792f1aacedc425c
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2022-12-08 21:55:23 -0700

    no se hacen las migraciones

---
+++ BlazorFarmacia/Server/Migrations/20221209040457_Clientes.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorFarmacia.Server.Migrations
+{
+    public partial class Clientes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ventas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdCliente = table.Column<int>(type: "int", nullable: false),
+                    TotalVenta = table.Column<int>(type: "int", nullable: false),
+                    Folio = table.Column<int>(type: "int", nullable: false),
+                    IdEmpleado = table.Column<int>(type: "int", nullable: false),
+                    IdProductosLotes = table.Column<int>(type: "int", nullable: false),
+                    Importe = table.Column<int>(type: "int", nullable: false),
+                    ValorUnitario = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ventas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VentasId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cliente_Ventas_VentasId",
+                        column: x => x.VentasId,
+                        principalTable: "Ventas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Empleados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VentasId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleados", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Empleados_Ventas_VentasId",
+                        column: x => x.VentasId,
+                        principalTable: "Ventas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductosLotes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdProducto = table.Column<int>(type: "int", nullable: false),
+                    IdLote = table.Column<int>(type: "int", nullable: false),
+                    VentasId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductosLotes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductosLotes_Ventas_VentasId",
+                        column: x => x.VentasId,
+                        principalTable: "Ventas",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lotes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Lote = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaCaducidad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductosLotesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lotes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Lotes_ProductosLotes_ProductosLotesId",
+                        column: x => x.ProductosLotesId,
+                        principalTable: "ProductosLotes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    Pasillo = table.Column<int>(type: "int", nullable: false),
+                    Fila = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PuntosCompra = table.Column<int>(type: "int", nullable: false),
+                    ProductosLotesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Productos_ProductosLotes_ProductosLotesId",
+                        column: x => x.ProductosLotesId,
+                        principalTable: "ProductosLotes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cliente_VentasId",
+                table: "Cliente",
+                column: "VentasId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empleados_VentasId",
+                table: "Empleados",
+                column: "VentasId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lotes_ProductosLotesId",
+                table: "Lotes",
+                column: "ProductosLotesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_ProductosLotesId",
+                table: "Productos",
+                column: "ProductosLotesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductosLotes_VentasId",
+                table: "ProductosLotes",
+                column: "VentasId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+
+            migrationBuilder.DropTable(
+                name: "Empleados");
+
+            migrationBuilder.DropTable(
+                name: "Lotes");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "ProductosLotes");
+
+            migrationBuilder.DropTable(
+                name: "Ventas");
+        }
+    }
+}
---
+++ BlazorFarmacia/Server/Migrations/20221209041726_ProductosLotes.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorFarmacia.Server.Migrations
+{
+    public partial class ProductosLotes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
